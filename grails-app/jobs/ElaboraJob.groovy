81b98e4e416a23d3370fcad4c890926367d43ba0
GuidoCeresa <gac@algos.it>
2014-07-04 06:56:34 +0200

    elaboraJob

---
+++ grails-app/jobs/ElaboraJob.groovy
@@ -0,0 +1,30 @@
+import it.algos.algosbio.LibBio
+import it.algos.algospref.Pref
+
+/**
+ * Created by gac on 04/07/14.
+ */
+class ElaboraJob {
+    // utilizzo di un service con la businessLogic per l'elaborazione dei dati
+    // il service viene iniettato automaticamente
+    def bioService
+
+    //--codifica dell'orario di attivazione
+    //--MON, TUE, WED, THU, FRI, SAT, SUN
+    //--tutti i giorni dalle 12 alle 11 di sera
+    private static String cronExpressionElabora = "0 0 12-23 ? * *"
+
+    static triggers = {
+        cron name: 'elabora', cronExpression: cronExpressionElabora
+    }// fine del metodo statico
+
+    def execute() {
+        //--flag di attivazione
+        if (Pref.getBool(LibBio.USA_CRONO_ELABORA)) {
+            if (bioService) {
+                bioService.elabora()
+            }// fine del blocco if
+        }// fine del blocco if
+    }// fine del metodo execute
+
+} // end of Job Class
